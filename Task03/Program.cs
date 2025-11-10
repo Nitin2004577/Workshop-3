9cc8c20feaced6527ead778a3d48309ba43ac93c
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-11-10 11:57:03 +0545

    Task 03 Completed

---
+++ Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age >= 13 && age <= 19)
+        {
+            Console.WriteLine("Teenager");
+        }
+        else
+        {
+            Console.WriteLine("Adult");
+        }
+    }
+}
